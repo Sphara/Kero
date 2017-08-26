f3a636d7493defd0fa56d46f5cd34cb545e6ebbe
Benoit Brunaud <benoit.brunaud@epitech.eu>
2017-08-26 20:57:23 +0200

    death & stuff

---
+++ Assets/Scripts/GUI/Death.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+    public Canvas DeathMenu;
+
+    public void Die()
+    {
+        DeathMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ChangeLevel(int level)
+    {
+        Time.timeScale = 1.0f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }
+}
