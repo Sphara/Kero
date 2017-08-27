2d4d550038065a7e25fdb844cf281d66bb1a0066
Benoit Brunaud <benoit.brunaud@epitech.eu>
2017-08-27 12:22:35 +0200

    actually playable

---
+++ Assets/Scripts/misc/BurrowingDoorInteractionScript1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurrowingDoorInteractionScript1 : InteractionScript {
+
+    public List<KeyInteractionScript> keyList;
+
+    public bool isBurrowing = false;
+
+    public override void Interact(GameObject InteractionAuthor)
+    {
+        foreach (KeyInteractionScript key in keyList)
+        {
+            if (key.state)
+                return;
+        }
+
+        isBurrowing = true;
+
+    }
+
+
+    private void Update()
+    {
+        if (isBurrowing)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 1);
+        }
+    }
+}
