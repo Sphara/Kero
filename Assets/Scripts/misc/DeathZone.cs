0db5ae7d09ab20ba68cfc55bbc9e56bfffde02b5
Benoit Brunaud <benoit.brunaud@epitech.eu>
2017-08-27 08:07:55 +0200

    added more death zones

---
+++ Assets/Scripts/misc/DeathZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+    public Vector3 startingPoint;
+    public Vector3 endPoint;
+
+    public Death death;
+
+    public LayerMask layer;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Physics2D.Linecast(startingPoint, endPoint, layer))
+            death.Die();
+	}
+}
